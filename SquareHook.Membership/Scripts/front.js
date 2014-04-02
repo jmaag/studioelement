51a670357368fc6315219dbb2e966f856cbdf36a
Joshua Maag <joshuamaag@gmail.com>
2014-04-02 17:04:48 -0600

    fixed popover to stay open so we can click on the link.

--- SquareHook.Membership/Scripts/front.js
+++ SquareHook.Membership/Scripts/front.js
@@ -155,8 +155,21 @@ SquareHook.Certification = (function ($) {
             $(".sh-details .levels li > a").popover({
                 html: true,
                 placement: 'right',
-                trigger: 'hover',
+                trigger: 'manual',
                 container: 'body'
+            }).on("mouseenter", function () {
+                var _this = this;
+                $(this).popover("show");
+                $(".popover").on("mouseleave", function () {
+                    $(_this).popover('hide');
+                });
+            }).on("mouseleave", function () {
+                var _this = this;
+                setTimeout(function () {
+                    if (!$(".popover:hover").length) {
+                        $(_this).popover("hide")
+                    }
+                }, 100);
             });
         }
     };
